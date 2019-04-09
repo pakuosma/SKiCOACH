487c72f077b4ef7f52aa1481a0f21ce70c295a2d
Elina <elina.kuosmanen@oulu.fi>
2019-04-09 10:20:56 +0300

    storyboard editing
    storyboard editing

--- SKiCOACH/ViewController.swift
+++ SKiCOACH/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var skatingStyleImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
+    @IBAction func skatingStyleButtons(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            skatingStyleImage.image = UIImage.init(named: "select-classic")
+            break
+        case 1:
+            skatingStyleImage.image = UIImage.init(named: "select-skate")
+            break
+        default:
+            break
+        }
+    }
+    
 }
 
